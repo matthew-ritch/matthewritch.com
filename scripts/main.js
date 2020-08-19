83d636e43b7499430450ced5e6f8ee0038b22e35
matthew-ritch <mritch3@gatech.edu>
2020-08-19 12:24:42 -0400

    base

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,6 @@ myImage.onclick = function() {
       myImage.setAttribute('src','https://i5.walmartimages.com/asr/87061092-70de-4a9b-ae77-b0f20a9c54d5_1.520c5864c2391e9f24041a1faa3d5d3a.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff');
     } else {
       myImage.setAttribute('src','images/user-image.png');
-    }
-}
+    };
+};
 
