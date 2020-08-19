77c7cc2aabfe1b0cebb8b4b6d1215b9e511108f9
matthew-ritch <mritch3@gatech.edu>
2020-08-19 12:19:22 -0400

    base

--- scripts/main.js
+++ scripts/main.js
@@ -9,27 +9,3 @@ myImage.onclick = function() {
     }
 }
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector("h2[name='welcome-bar']");
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-  	setUserName();
-  } else {
-
-  } localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome, ' + myName + '!';
-  
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Welcome, ' + storedName + '!';
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
